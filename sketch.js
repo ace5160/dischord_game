1048e86ce0f2ad0b780f952b9d7d9943f1c70863
ace5160 <shikhar_j@nid.edu>
2018-11-19 15:58:41 -0500

    added multiple ships
    pubnub not working, going for a different approach to get it working

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@
 
 var bullets;
 var asteroids;
-var ship;
+var ship1,ship2,ship3,ship4,ship5;
 var blocks;
 var ctr = 0;
 var shipImage, bulletImage, particleImage;
@@ -18,10 +18,30 @@ function setup() {
   shipImage = loadImage('assets/asteroids_ship0001.png');
   particleImage = loadImage('assets/asteroids_particle.png');
 
-  ship = createSprite(width/2, height/2);
-  ship.maxSpeed = 600;
-  ship.friction = 0.98;
-  ship.setCollider('circle', 0, 0, 20);
+  ship1 = createSprite(width/2, height/2);
+  ship1.maxSpeed = 600;
+  ship1.friction = 0.98;
+  ship1.setCollider('circle', 0, 0, 20);
+  
+  ship2 = createSprite(width/4, height/4);
+  ship2.maxSpeed = 600;
+  ship2.friction = 0.98;
+  ship2.setCollider('circle', 0, 0, 20);
+  
+  ship3 = createSprite(width/6, height/6);
+  ship3.maxSpeed = 600;
+  ship3.friction = 0.98;
+  ship3.setCollider('circle', 0, 0, 20);
+  
+  ship4 = createSprite(width-(width/4), height-(height/5));
+  ship4.maxSpeed = 600;
+  ship4.friction = 0.98;
+  ship4.setCollider('circle', 0, 0, 20);
+  
+  ship5 = createSprite(width-(width/3), height-(height/3));
+  ship5.maxSpeed = 600;
+  ship5.friction = 0.98;
+  ship5.setCollider('circle', 0, 0, 20);
 
 block1 = createSprite(0, windowHeight/5, 200, 50);
 block2 = createSprite(200, 2 * windowHeight/5, 80, 80);
@@ -37,15 +57,26 @@ block9 = createSprite(windowWidth-200, 2*(windowHeight)/5, 200, 50);
 //top wall blocks
 block10 = createSprite(windowWidth/3, 80, 250, 50);
 block11 = createSprite(windowWidth-300, 0, 50, 100);
-
-//centre shrine
+  //centre shrine
 shrine1 = createSprite((windowWidth/2)+100, (windowHeight/2)+50, 120, 20);
 shrine2 = createSprite((windowWidth/2)+100, (windowHeight/2)-50, 120, 20);
 shrine3 = createSprite((windowWidth/2)+30, (windowHeight/2), 20, 120);
 shrine4 = createSprite((windowWidth/2)+150, (windowHeight/2), 20, 120);
 
-  ship.addImage('normal', shipImage);
-  ship.addAnimation('thrust', 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+  ship1.addImage('normal', shipImage);
+  ship1.addAnimation('thrust', 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+  
+  ship2.addImage('normal', shipImage);
+  ship2.addAnimation('thrust', 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+  
+  ship3.addImage('normal', shipImage);
+  ship3.addAnimation('thrust', 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+  
+  ship4.addImage('normal', shipImage);
+  ship4.addAnimation('thrust', 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
+  
+  ship5.addImage('normal', shipImage);
+  ship5.addAnimation('thrust', 'assets/asteroids_ship0002.png', 'assets/asteroids_ship0007.png');
 
   asteroids = new Group();
   bullets = new Group();
@@ -57,6 +88,7 @@ shrine4 = createSprite((windowWidth/2)+150, (windowHeight/2), 20, 120);
     var py = height/2+ 1000 * sin(radians(ang));
     createAsteroid(3, px, py);
   }
+  
 }
 
 function draw() {
@@ -73,21 +105,22 @@ function draw() {
     if(s.position.y<-MARGIN) s.position.y = height+MARGIN;
     if(s.position.y>height+MARGIN) s.position.y = -MARGIN;
   }
+ 
 
   asteroids.overlap(bullets, asteroidHit);
-  ship.bounce(asteroids);
+  ship1.bounce(asteroids);
   //ship and block collisions
-  ship.collide(block1);
-  ship.collide(block2);
-  ship.collide(block3);
-  ship.collide(block4);
-  ship.collide(block5);
-  ship.collide(block6);
-  ship.collide(block7);
-  ship.collide(block8);
-  ship.collide(block9);
-  ship.collide(block10);
-  ship.collide(block11);
+  ship1.collide(block1);
+  ship1.collide(block2);
+  ship1.collide(block3);
+  ship1.collide(block4);
+  ship1.collide(block5);
+  ship1.collide(block6);
+  ship1.collide(block7);
+  ship1.collide(block8);
+  ship1.collide(block9);
+  ship1.collide(block10);
+  ship1.collide(block11);
   //asteroid and block bouncing
   asteroids.bounce(block1);
   asteroids.bounce(block2);
@@ -101,10 +134,10 @@ function draw() {
   asteroids.bounce(block10);
   asteroids.bounce(block11);
   //ship and shrine collision
-  ship.collide(shrine1);
-  ship.collide(shrine2);
-  ship.collide(shrine3);
-  ship.collide(shrine4);
+  ship1.collide(shrine1);
+  ship1.collide(shrine2);
+  ship1.collide(shrine3);
+  ship1.collide(shrine4);
 asteroids.collide(shrine1);
 asteroids.collide(shrine2);
 asteroids.collide(shrine3);
@@ -112,36 +145,33 @@ asteroids.collide(shrine4);
 
 
   if(keyDown(LEFT_ARROW))
-    ship.rotation -= 4;
+    ship1.rotation -= 4;
   if(keyDown(RIGHT_ARROW))
-    ship.rotation += 4;
+    ship1.rotation += 4;
   if(keyDown(UP_ARROW))
   {
-    ship.addSpeed(200, ship.rotation);
-    ship.changeAnimation('thrust');
+    ship1.addSpeed(200, ship1.rotation);
+    ship1.changeAnimation('thrust');
   }
   else
-    ship.changeAnimation('normal');
+    ship1.changeAnimation('normal');
 
   if(keyWentDown('x'))
   {
-    var bullet = createSprite(ship.position.x, ship.position.y);
+    var bullet = createSprite(ship1.position.x, ship1.position.y);
     bullet.addImage(bulletImage);
-    bullet.setSpeed(30, ship.rotation);
+    bullet.setSpeed(30, ship1.rotation);
     bullet.life = 30;
     bullets.add(bullet);
     
     //bullethitshrine
-if(bullet.overlap(shrine1))
+if(bullets.overlap(shrine1))
 {
-    ctr++;
+    removeSprite(shrine1);
 }
   }
   drawSprites();
-  if(ctr>2)
-  {
-      shrine1.remove();
-  }
+  
 
 }
 
@@ -183,4 +213,12 @@ function asteroidHit(asteroid, bullet) {
 
   bullet.remove();
   asteroid.remove();
+}
+
+function mousePressed() {
+  noLoop();
+}
+
+function mouseReleased() {
+  loop();
 }
